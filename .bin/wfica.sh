aae63cd3a392c39caf40ec79d3645198f8a4d908
Martin Pöhlmann <mpdeimos@users.noreply.github.com>
2018-03-15 08:08:24 +0100

    extended homeoffice script

--- .bin/wfica.sh
+++ .bin/wfica.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
+if [[ ! -s ~/wfica.csv ]]
+then
+	echo "Date      ;SSID" > ~/wfica.csv
+fi
+loginDate=`date -I`
+loginSsid=`nmcli -t -f active,ssid dev wifi | egrep '^yes' | cut -d: -f2`
+line="$loginDate;$loginSsid"
+last=`cat ~/wfica.csv | tail -1`
+if [[ "$line" != "$last" ]]
+then
+	echo  $line >> ~/wfica.csv
+fi
+
 export ICAROOT=/opt/Citrix/ICAClient
-date -I >> ~/wfica.log
 ${ICAROOT}/wfica -file "$1"
